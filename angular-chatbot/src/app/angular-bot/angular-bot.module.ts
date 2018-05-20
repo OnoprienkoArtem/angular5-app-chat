0ef6400b180e5aa6a0fcffb93cbccf400e9873b3
OnoprienkoArtem <o.artem.e@gmail.com>
2018-05-20 17:56:07 +0300

    setup chat service

--- angular-chatbot/src/app/angular-bot/angular-bot.module.ts
+++ angular-chatbot/src/app/angular-bot/angular-bot.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import { ChatComponent } from './chat/chat.component';
+import { ChatService } from './chat.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [MainComponent, ChatComponent]
+  declarations: [MainComponent, ChatComponent],
+  providers: [ChatService]
 })
 export class AngularBotModule { }
